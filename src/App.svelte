620dc5a98302008a41dc2d3cc3fa5a8f06a0e408
Sam Claus <samclaus@protonmail.com>
2023-05-22 08:52:13 -0400

    Add favorite stops modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
     import ModalContainer, { fire } from "./lib/ModalContainer.svelte";
     import { leaflet } from "./lib/leaflet.action";
+    import FavoriteStopsModal from "./lib/modals/FavoriteStopsModal.svelte";
     import RoutesModal from "./lib/modals/RoutesModal.svelte";
 </script>
 
 <main>
     <h1 class="sr-only">RTS Bus Tracker</h1>
     <div class="map" use:leaflet></div>
-    <button
-        title="Select routes"
-        aria-label="Select routes"
-        on:click={() => fire(RoutesModal, {})}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M11,10H5L3,8L5,6H11V3L12,2L13,3V4H19L21,6L19,8H13V10H19L21,12L19,14H13V20A2,2 0 0,1 15,22H9A2,2 0 0,1 11,20V10Z" />
-        </svg>
-    </button>
+    <div class="tools">
+        <button
+            title="Select routes"
+            aria-label="Select routes"
+            on:click={() => fire(RoutesModal, {})}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M11,10H5L3,8L5,6H11V3L12,2L13,3V4H19L21,6L19,8H13V10H19L21,12L19,14H13V20A2,2 0 0,1 15,22H9A2,2 0 0,1 11,20V10Z" />
+            </svg>
+        </button>
+        <button
+            title="Favorite stops"
+            aria-label="Favorite stops"
+            on:click={() => fire(FavoriteStopsModal, {})}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M12 2C8.1 2 5 5.1 5 9C5 14.2 12 22 12 22S19 14.2 19 9C19 5.1 15.9 2 12 2M14.5 13L12 11.5L9.5 13L10.2 10.2L8 8.3L10.9 8.1L12 5.4L13.1 8L16 8.3L13.8 10.2L14.5 13Z" />
+            </svg>
+        </button>
+    </div>
 </main>
 
 <ModalContainer />
@@ -31,11 +42,17 @@
         isolation: isolate;
     }
 
-    button {
+    .tools {
         position: absolute;
         bottom: 24px;
         right: 24px;
 
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+
+    button {
         padding: 8px;
     }
 
---
+++ src/lib/modals/FavoriteStopsModal.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<h2>
+    Favorite stops
+</h2>
+
+<div class="placeholder">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M12 2C8.1 2 5 5.1 5 9C5 14.2 12 22 12 22S19 14.2 19 9C19 5.1 15.9 2 12 2M14.5 13L12 11.5L9.5 13L10.2 10.2L8 8.3L10.9 8.1L12 5.4L13.1 8L16 8.3L13.8 10.2L14.5 13Z" />
+    </svg>
+    <h3>
+        You do not have any favorite stops
+    </h3>
+    <p>
+        To favorite a stop, select it on the map and click the star icon.
+    </p>
+</div>

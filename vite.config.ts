60e5a7853682651581f61231c51dfb27fffe4546
Sam Claus <samclaus@protonmail.com>
2023-07-23 18:56:44 -0400

    Add rollup-plugin-visualizer to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+    plugins: [
+        svelte(),
+        visualizer({
+            template: 'treemap', // or sunburst
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+            filename: 'analyse.html', // will be saved in project's root
+        }),
+    ],
+});

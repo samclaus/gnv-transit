ba60cf6391b89a4860e5186b411edc3375ecb08f
Sam Claus <samclaus@protonmail.com>
2023-08-08 11:59:24 -0400

    Make it a PWA!

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,25 @@ export default defineConfig({
             gzipSize: true,
             brotliSize: true,
             filename: 'analyse.html', // will be saved in project's root
+        }),
+        VitePWA({
+            injectRegister: 'inline',
+            registerType: 'autoUpdate',
+            manifest: {
+                background_color: '#fcf8f4',
+                categories: ['transit', 'maps'],
+                description: 'GNV Transit is an app for tracking RTS bus times in Gainesville, FL.',
+                display: 'fullscreen',
+                display_override: ['fullscreen', 'standalone', 'browser'],
+                icons: [{
+                    src: '/logo.svg',
+                    sizes: 'any',
+                }],
+                name: 'GNV Transit',
+                orientation: 'portrait-primary',
+                short_name: 'Transit',
+                theme_color: '#fff',
+            },
         }),
     ],
     optimizeDeps: {
